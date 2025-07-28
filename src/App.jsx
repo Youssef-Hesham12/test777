0f5ce0cdf7f8c817d63d9f51f47b318d4834a391
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-07-28 23:12:54 +0300

    two

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Section from './components/Section/Section'
-import Footer from './components/Footer/footer'
+import Footer from './components/Footer/Footer'
 import About from './pages/About/About'
 import Portfoli from './pages/Portfoli/Portfoli'
 import Layout from './components/Layout/Layout';
